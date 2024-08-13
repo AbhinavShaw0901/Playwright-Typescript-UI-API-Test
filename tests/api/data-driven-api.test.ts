4e9bac21d84a1ab179f5a3a387d52f994f0f969d
princesoni22 <prince.soni@nirmata.com>
2024-08-14 02:07:44 +0530

    added data driven test

---
+++ tests/api/data-driven-api.test.ts
@@ -0,0 +1,24 @@
+import { test, expect } from "@playwright/test";
+import dataDriven from "../../test-data/data-driven-api.json";
+import { APICommon } from "../../lib/api-common";
+const apiCommon = new APICommon();
+
+dataDriven.map((ele) => {
+  test(
+    `POST REQUEST - Register multiple users with email -> ${ele.email} and password -> ${ele.password} stored in a json file`,
+    { tag: "@api" },
+    async ({ request }) => {
+      const response = await request.post("/api/register", {
+        data: {
+          email: ele.email,
+          password: ele.password,
+        },
+      });
+      const respBody = JSON.parse(await response.text());
+      console.log(respBody);
+      apiCommon.getHeaderValues(response);
+      apiCommon.validateResponseCode(response, 200);
+      expect(respBody).toHaveProperty("token");
+    }
+  );
+});
